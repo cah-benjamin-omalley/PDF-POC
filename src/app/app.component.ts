4645c7e79ea0e4f294f130dd32d2925449c2974c
Ben O'Malley <benjamin.omalley@cardinalhealth.com>
2022-09-13 16:08:40 -0400

    PDF viewer POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-app';
+  src = 'assets/pdfs/Bootstrap-vs-Material-Design-vs-Prime-vs-Tailwind.pdf';
 }
